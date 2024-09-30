2c50af6030d2e046f403ba79dcf7825626139eaa
pujiarto <pujiarto.ta@gmail.com>
2024-09-30 12:24:37 +0800

    modified:   e_absen_backend/__pycache__/serializers.cpython-312.pyc 	modified:   e_absen_backend/serializers.py

--- e_absen_backend/serializers.py
+++ e_absen_backend/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from e_absen_backend.models.model_user import User_Id
+from rest_framework import generics, status
+from django.http import JsonResponse
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -29,3 +31,21 @@ class UserIdSerializer(serializers.ModelSerializer):
         user = UserSerializer().create(user_data)
         user_id = User_Id.objects.create(user_id=user, **validated_data)
         return user_id
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserIdSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user_profile = serializer.save()
+            return JsonResponse({
+                'user': {
+                    'email': user_profile.user.email,  # Access user attribute correctly
+                },
+                'message': 'User registered successfully.'
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse({
+                'errors': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
